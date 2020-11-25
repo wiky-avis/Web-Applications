eeee29c9299109566a12d61d46c0f38f94039192
wiky-avis <peta-holding@yandex.ru>
2020-11-25 17:41:59 +0300

    add app learn_logs

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\manager3\documents\vscode-exercise\dev\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
