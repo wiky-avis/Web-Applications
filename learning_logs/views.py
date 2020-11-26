395682688a71b7e0b1aa0658f2d3ec309df76054
wiky-avis <peta-holding@yandex.ru>
2020-11-26 18:26:43 +0300

    add

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
+def index(request):
+    """Домашняя страница learning log"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Выводит список тем"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Выводит одну тему и все ее записи."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
